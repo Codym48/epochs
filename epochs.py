0aec69cca12f04743fe8416026bb583781cc306e
Cody Martin <codytylermartin@gmail.com>
2023-10-08 20:53:30 -0700

    Add comments about time systems

--- epochs.py
+++ epochs.py
@@ -9,12 +9,18 @@ import spiceypy
 # (https://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/)
 spiceypy.furnsh("naif0012.tls.pc")
 
+# Some notes on time systems:
+#   UTC = TAI - 37s (after 1 January 2017)
+#   TAI = TT - 32.184s
+#   TT == TDT
+#   ET == TDB = TT + 0.001658s * sin(E)
+
 # What is the J2000 epoch used by SPICE?
 j2000_in_tdb = spiceypy.timout(0, "YYYY-MM-DDTHR:MN:SC.############ ::TDB")
 j2000_in_utc = spiceypy.timout(0, "YYYY-MM-DDTHR:MN:SC.############ ::UTC")
 j2000_in_tt = spiceypy.timout(0, "YYYY-MM-DDTHR:MN:SC.############ ::TT")
 
-# Covert the (?) Unix epoch into TDB
+# Convert the Unix time epoch into TDB
 unix_s_past_j2000 = spiceypy.str2et("00:00:00 UTC 1 January 1970")
 unix_in_tdb = spiceypy.timout(
     unix_s_past_j2000, "YYYY-MM-DDTHR:MN:SC.############ ::TDB"
@@ -27,5 +33,5 @@ unix_in_tt = spiceypy.timout(unix_s_past_j2000, "YYYY-MM-DDTHR:MN:SC.###########
 # (https://en.wikipedia.org/wiki/Unix_time#Variant_that_counts_leap_seconds) "Because
 # TAI has no leap seconds, and every TAI day is exactly 86400 seconds long, this
 # encoding is actually a pure linear count of seconds elapsed since
-# 1970-01-01T00:00:10 TAI"
-unix_variant = "1970-01-01T00:00:10"
+# 1970-01-01T00:00:10 TAI (1970-01-01T00:00:00 UTC)"
+print(-unix_s_past_j2000)
